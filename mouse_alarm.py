bcd9fba9b0c67530cb5afddae0c73ba9971f1f79
₱ØⱠɎ₥ØⱤ₱Ⱨł₵ <54730301+lilmond@users.noreply.github.com>
2024-11-15 16:57:25 +0800

    Update mouse_alarm.py

--- mouse_alarm.py
+++ mouse_alarm.py
@@ -1,8 +1,3 @@
-# This script plays an alarm sound effect when your mouse has been moved.
-# Budget security system for protecting your base from robbers.
-# Just attach a string to your mouse and your door.
-# Don't forget to download Alarm sound effect.mp3
-
 # Requiresments:
 # pynput
 # soundfile
@@ -11,7 +6,20 @@
 from pynput import mouse
 import sounddevice as sd
 import soundfile as sf
+import threading
 import time
+
+ALARM_ONCE = False
+
+def play_alarm():
+    while True:
+        file_path = "Alarm sound effect.mp3"
+        data, fs = sf.read(file_path)
+        sd.play(data, fs)
+        sd.wait()
+
+        if ALARM_ONCE:
+            break
 
 def main():
     mouse_controller = mouse.Controller()
@@ -23,10 +31,13 @@ def main():
 
         time.sleep(0.05)
 
-    file_path = "Alarm sound effect.mp3"
-    data, fs = sf.read(file_path)
-    sd.play(data, fs)
-    sd.wait()
+    if ALARM_ONCE:
+        play_alarm()
+    else:
+        threading.Thread(target=play_alarm, daemon=True).start()
+
+        input("- Security system has been triggered. Press ENTER to stop the alarm. -")
+
 
 if __name__ == "__main__":
     main()
